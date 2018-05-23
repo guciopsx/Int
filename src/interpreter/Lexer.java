cc60ab8f395b8bc01d4bec0d8de45bbda3b12b86
Gucio <guciopsx@gmail.com>
2018-05-23 23:36:28 +0200

    tabela tokenow w1

--- src/interpreter/Lexer.java
+++ src/interpreter/Lexer.java
@@ -4,11 +4,26 @@ import java.util.ArrayList;
 
 public class Lexer extends Exception{
     private ArrayList<Token> tokeny = new ArrayList<>();
+    private int wskaznik;
 
     Lexer(String input){
-        for(int i=0; i<input.length();i++){
-            tokeny.add(new Token(String.valueOf(input.charAt(i)),null));
+        for(wskaznik=0; wskaznik<input.length(); wskaznik++){
+            if(input.charAt(wskaznik)=='"'){
+                wskaznik++;
+                tokeny.add(new Token("String", scanString(input)));
+            }
+            tokeny.add(new Token(String.valueOf(input.charAt(wskaznik)),null));
         }
+    }
+
+    private String scanString(String input){
+        String text = "";
+        while(input.charAt(wskaznik)!='"' && wskaznik<input.length()){
+            text+=input.charAt(wskaznik);
+            wskaznik++;
+        }
+        wskaznik++;
+        return text;
     }
 
     public ArrayList<Token> getTokeny() {
